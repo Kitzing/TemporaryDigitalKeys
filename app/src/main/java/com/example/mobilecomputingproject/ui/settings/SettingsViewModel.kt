e7bf16c9642913c6b58037a2342d3df1cbad738a
Kitzing <sara.kitzing@hotmail.com>
2020-10-14 10:15:51 +0200

    name changes

--- app/src/main/java/com/example/mobilecomputingproject/ui/settings/SettingsViewModel.kt
+++ app/src/main/java/com/example/mobilecomputingproject/ui/settings/SettingsViewModel.kt
@@ -1,4 +1,4 @@
-package com.example.mobilecomputingproject.ui.notifications
+package com.example.mobilecomputingproject.ui.settings
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
