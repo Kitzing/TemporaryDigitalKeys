97bd07c7e76e62ceda98e4521cf532b1b7ddf6fa
Kitzing <sara.kitzing@hotmail.com>
2020-10-19 16:24:15 +0200

    Added button click to receive

--- app/src/main/java/com/example/mobilecomputingproject/ui/receive/ReceiveAdapter.kt
+++ app/src/main/java/com/example/mobilecomputingproject/ui/receive/ReceiveAdapter.kt
@@ -3,22 +3,39 @@ package com.example.mobilecomputingproject.ui.receive
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.mobilecomputingproject.R
 import kotlinx.android.synthetic.main.receive_list_item.view.*
 
-class ReceiveAdapter (private val receiveList: List<ReceiveListItem> ): RecyclerView.Adapter<ReceiveAdapter.ReceiveViewHolder>() {
+class ReceiveAdapter (private val receiveList: List<ReceiveListItem>, private val listener: ReceiveAdapter.OnButtonClickListener): RecyclerView.Adapter<ReceiveAdapter.ReceiveViewHolder>() {
 
 
 
-    class ReceiveViewHolder (itemView : View) : RecyclerView.ViewHolder (itemView) {
+    inner class ReceiveViewHolder (itemView : View) : RecyclerView.ViewHolder (itemView), View.OnClickListener {
         val imageView: ImageView = itemView.receive_image_view
         val name: TextView = itemView.receive_text_view_name
         val address: TextView = itemView.receive_text_view_address
         val date: TextView = itemView.receive_text_view_date
+        val button: Button = itemView.open_button
 
+        init {
+            button.setOnClickListener(this)
+        }
+
+        override fun onClick(v: View?) {
+            val position = adapterPosition
+            if (position != RecyclerView.NO_POSITION) {
+                listener.onButtonClick(position, button)
+            }
+        }
+
+    }
+
+    interface OnButtonClickListener{
+        fun onButtonClick(position: Int, button: Button)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ReceiveViewHolder {
--- app/src/main/java/com/example/mobilecomputingproject/ui/receive/ReceiveFragment.kt
+++ app/src/main/java/com/example/mobilecomputingproject/ui/receive/ReceiveFragment.kt
@@ -1,10 +1,13 @@
 package com.example.mobilecomputingproject.ui.receive
 
+import android.graphics.Color
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
@@ -15,7 +18,7 @@ import com.example.mobilecomputingproject.ui.lend.LendViewModel
 import kotlinx.android.synthetic.main.fragment_lend.view.*
 import kotlinx.android.synthetic.main.fragment_receive.view.*
 
-class ReceiveFragment : Fragment() {
+class ReceiveFragment : Fragment(), ReceiveAdapter.OnButtonClickListener {
 
     private lateinit var receiveViewModel: ReceiveViewModel
 
@@ -27,15 +30,27 @@ class ReceiveFragment : Fragment() {
         receiveViewModel =
             ViewModelProviders.of(this).get(ReceiveViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_receive, container, false)
-     //   val textView: TextView = root.findViewById(R.id.text_dashboard)
-     //   dashboardViewModel.text.observe(viewLifecycleOwner, Observer {
-     //       textView.text = it
-     //   })
+        //   val textView: TextView = root.findViewById(R.id.text_dashboard)
+        //   dashboardViewModel.text.observe(viewLifecycleOwner, Observer {
+        //       textView.text = it
+        //   })
 
-        root.receive_list.adapter = ReceiveAdapter(ReceiveViewModel().exampleList)
+        root.receive_list.adapter = ReceiveAdapter(ReceiveViewModel().exampleList, this)
         root.receive_list.layoutManager = LinearLayoutManager(this.context)
         root.receive_list.setHasFixedSize(true)
 
         return root
     }
+
+    override fun onButtonClick(position: Int, button: Button) {
+        val clickedItem = LendViewModel().exampleList[position]
+        val name = clickedItem.name
+        Toast.makeText(
+            this.context,
+            "$name is now activated, please hold your phone against the receiver",
+            Toast.LENGTH_LONG
+        ).show()
+        //TODO Launch NFC
+    }
+
 }
