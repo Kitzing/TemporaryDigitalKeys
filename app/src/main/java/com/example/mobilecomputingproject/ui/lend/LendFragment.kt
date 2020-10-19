4c18bd36ef44ce2a6ed4d7a5e168ce0c3be133df
Kitzing <sara.kitzing@hotmail.com>
2020-10-19 10:38:55 +0200

    Added connection between lend and choose

--- app/src/main/java/com/example/mobilecomputingproject/ui/lend/LendFragment.kt
+++ app/src/main/java/com/example/mobilecomputingproject/ui/lend/LendFragment.kt
@@ -4,8 +4,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProviders
+import androidx.navigation.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.mobilecomputingproject.R
 import kotlinx.android.synthetic.main.fragment_lend.view.*
@@ -34,12 +36,17 @@ class LendFragment : Fragment() {
         root.lend_list.setHasFixedSize(true)
 
 
-
+            root.lend_key_button.setOnClickListener{
+                view : View ->
+            view.findNavController().navigate(R.id.action_navigation_home_to_chooseFragment)
+        }
 
             return root
         }
 
 
+
     }
+
 
 
