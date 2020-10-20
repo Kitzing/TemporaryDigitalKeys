2f5319878c754981e31e42db27c6647525d84fdd
Kitzing <sara.kitzing@hotmail.com>
2020-10-20 11:29:39 +0200

    Lite mer hel kod kanske

--- app/src/main/java/com/example/mobilecomputingproject/ui/settings/SettingsFragment.kt
+++ app/src/main/java/com/example/mobilecomputingproject/ui/settings/SettingsFragment.kt
@@ -2,6 +2,7 @@ package com.example.mobilecomputingproject.ui.settings
 
 import SettingsViewModelFactory
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -26,46 +27,58 @@ import kotlinx.android.synthetic.main.fragment_settings.view.*
 
 class SettingsFragment : Fragment(), KeyAdapter.OnButtonClickListener {
 
-    private lateinit var notificationsViewModel: SettingsViewModel
+   // private lateinit var notificationsViewModel: SettingsViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        notificationsViewModel =
-            ViewModelProviders.of(this).get(SettingsViewModel::class.java)
+        Log.d("Help", "0")
+        //notificationsViewModel =
+         //   ViewModelProviders.of(this).get(SettingsViewModel::class.java)
+        Log.d("Help", "0.1")
         val root = inflater.inflate(R.layout.fragment_settings, container, false)
        // val textView: TextView = root.findViewById(R.id.text_notifications)
        // notificationsViewModel.text.observe(viewLifecycleOwner, Observer {
        //     textView.text = it
        // })
 
+        Log.d("Help", "1")
 
-
-        val application = requireNotNull(this.activity).application
+        val application = requireNotNull().application
 
         val dataSource = KeyDatabase.getInstance(application).KeyDatabaseDao
         val viewModelFactory = SettingsViewModelFactory(dataSource, application)
 
+        Log.d("Help", "2")
+
         val settingsViewModel =
             ViewModelProvider(
                 this, viewModelFactory).get(SettingsViewModel::class.java)
+
+        Log.d("Help", "3")
 
         val adapter = KeyAdapter(this)
         root.your_keys_list.adapter = adapter
         root.your_keys_list.layoutManager = LinearLayoutManager(this.context)
         root.your_keys_list.setHasFixedSize(true)
 
+        Log.d("Help", "4")
+
         settingsViewModel.keys.observe(viewLifecycleOwner, Observer { it?.let{
             adapter.data = it
         }
         })
 
+        Log.d("Help", "5")
+
 
         root.save_button.setOnClickListener{
                 Toast.makeText(this.context, "Your settings are saved", Toast.LENGTH_SHORT)
         }
+
+        Log.d("Help", "6")
 
         return root
     }
